678e654b5c79c421110ada168a0a2ea5a64e4a79
chinyanga <chinyangaf@gmail.com>
2020-06-24 19:56:42 +0200

    rename variable to match the class variables

--- util/__init__.py
+++ util/__init__.py
@@ -60,7 +60,7 @@ class DistanceCalculator:
                 elif j == 0: 
                     dp[i][j] = i 
 
-                elif str1[i-1] == str2[j-1]: 
+                elif source[i-1] == target[j-1]: 
                     dp[i][j] = dp[i-1][j-1] 
                 else: 
                     dp[i][j] = 1 + min(dp[i][j-1],	 # Insert 
@@ -69,7 +69,7 @@ class DistanceCalculator:
 
         return dp[m][n] 
 
-str1 = input("Enter first string ")
-str2 = input("Enter second string ")
+    source = input("Enter first string ")
+    target = input("Enter second string ")
 
-print(distance(str1, str2, len(str1), len(str2))) 
+    print(distance(source, target, len(source), len(target))) 
